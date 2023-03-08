886f9a5cae0ace619a4e62073f253274328ba24d
nicobell <nico.bellucco@gmail.com>
2023-03-08 16:14:23 +0100

    config cex install all Bootstrap modules, UI patterns and Display Suite

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# bootstrap_italia_tv
+# Bootstrap Italia Interfase
 Prova installazione e templating tema Bootstrap Italia
 
 docs ufficiali con gli step:
@@ -6,36 +6,44 @@ docs ufficiali con gli step:
 https://github.com/italia/design-drupal-theme
 
 
-## installazione node_modules
+## Installazione node_modules
 "node_modules" del tema non versionati perchè troppo pesanti, 
 includono tutte le dipendenze delle librerie di bootstrap e bootstrap_italia
 per installare da shell git:
 
-$ cd web/themes/custom/italiagov
-$ npm install
+```
+cd web/themes/custom/italiagov
+npm install
+```
 
 
 ## svg-inline-loader rule
 Problema per webpack nel fare il bundle di immagini svg inline nel codice,
 risolto con il modulo
 
-$ npm install svg-inline-loader --save-dev
+```
+npm install svg-inline-loader --save-dev
+```
 
 Poi serve aggiungere la seguente regola a module.loaders.rules in 
 webpack.dev.js e webpack.prod.js
 
-$ {
-$    test: /\.svg$/,
-$    loader: 'svg-inline-loader'
-$ }
+```
+{
+   test: /\.svg$/,
+   loader: 'svg-inline-loader'
+}
+```
 
 https://www.npmjs.com/package/svg-inline-loader
 
 
-## watch scss
+## Watch scss
 Durante sviluppo insieme a Docker, lanciare una shell git con
 
-$ npm run watch:dev
+```
+npm run watch:dev
+```
 
 Che compila scss in tempo reale nel css letto da Drupal,
 serve comunque fare "drush cr" per vedere i cambiamenti
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,17 +11,17 @@ class ComposerStaticInit57e3f4defb1bd4e005e1ea7e15ad4f0f
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         'e69f7f6ee287b969198c3c9d6777bd38' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/bootstrap.php',
-        '8825ede83f2f289127722d4e842cf7e8' => __DIR__ . '/..' . '/symfony/polyfill-intl-grapheme/bootstrap.php',
-        'b6b991a57620e2fb6b2f66f03fe9ddc2' => __DIR__ . '/..' . '/symfony/string/Resources/functions.php',
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
         'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
+        '8825ede83f2f289127722d4e842cf7e8' => __DIR__ . '/..' . '/symfony/polyfill-intl-grapheme/bootstrap.php',
+        'b6b991a57620e2fb6b2f66f03fe9ddc2' => __DIR__ . '/..' . '/symfony/string/Resources/functions.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
-        '23c18046f52bef3eea034657bafda50f' => __DIR__ . '/..' . '/symfony/polyfill-php81/bootstrap.php',
         'def43f6c87e4f8dfd0c9e1b1bab14fe8' => __DIR__ . '/..' . '/symfony/polyfill-iconv/bootstrap.php',
         '2f69d3914119f042cca9e44442d5ce95' => __DIR__ . '/../..' . '/web/core/includes/bootstrap.inc',
+        '23c18046f52bef3eea034657bafda50f' => __DIR__ . '/..' . '/symfony/polyfill-php81/bootstrap.php',
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
         '801c31d8ed748cfa537fa45402288c95' => __DIR__ . '/..' . '/psy/psysh/src/functions.php',
     );
@@ -109,13 +109,16 @@ class ComposerStaticInit57e3f4defb1bd4e005e1ea7e15ad4f0f
         'D' => 
         array (
             'Drush\\' => 6,
+            'Drupal\\ui_patterns_settings_settings\\' => 37,
             'Drupal\\Core\\' => 12,
             'Drupal\\Composer\\Plugin\\Scaffold\\' => 32,
             'Drupal\\Composer\\Plugin\\ProjectMessage\\' => 38,
             'Drupal\\Component\\' => 17,
             'DrupalCodeGenerator\\' => 20,
+            'Doctrine\\Persistence\\' => 21,
             'Doctrine\\Common\\Lexer\\' => 22,
             'Doctrine\\Common\\Annotations\\' => 28,
+            'Doctrine\\Common\\' => 16,
             'Dflydev\\DotAccessData\\' => 22,
         ),
         'C' => 
@@ -350,6 +353,10 @@ class ComposerStaticInit57e3f4defb1bd4e005e1ea7e15ad4f0f
         array (
             0 => __DIR__ . '/..' . '/drush/drush/src',
         ),
+        'Drupal\\ui_patterns_settings_settings\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/web/modules/contrib/ui_patterns_settings/src',
+        ),
         'Drupal\\Core\\' => 
         array (
             0 => __DIR__ . '/../..' . '/web/core/lib/Drupal/Core',
@@ -370,6 +377,10 @@ class ComposerStaticInit57e3f4defb1bd4e005e1ea7e15ad4f0f
         array (
             0 => __DIR__ . '/..' . '/chi-teck/drupal-code-generator/src',
         ),
+        'Doctrine\\Persistence\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/persistence/src/Persistence',
+        ),
         'Doctrine\\Common\\Lexer\\' => 
         array (
             0 => __DIR__ . '/..' . '/doctrine/lexer/lib/Doctrine/Common/Lexer',
@@ -377,6 +388,11 @@ class ComposerStaticInit57e3f4defb1bd4e005e1ea7e15ad4f0f
         'Doctrine\\Common\\Annotations\\' => 
         array (
             0 => __DIR__ . '/..' . '/doctrine/annotations/lib/Doctrine/Common/Annotations',
+        ),
+        'Doctrine\\Common\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/doctrine/common/src',
+            1 => __DIR__ . '/..' . '/doctrine/event-manager/src',
         ),
         'Dflydev\\DotAccessData\\' => 
         array (
